6ed08a124e5ac81bc519e58c2c66c16b31c2b93b
Aynurmm <aynurmammadova09@gmail.com>
2024-10-26 18:03:15 +0400

    operators

--- src/main/java/az/academy/turing/Main.java
+++ src/main/java/az/academy/turing/Main.java
@@ -7,5 +7,7 @@ byte a1=9;
         System.out.print(a1+"\n");
         System.out.println(a1);
         System.out.printf("%d" , a1);
+
+
     }
 }
---
+++ src/main/java/az/academy/turing/Operators.java
@@ -0,0 +1,17 @@
+package az.academy.turing;
+
+public class Operators {
+    int num1=12;
+    int num2=4;
+    int cem=num1+num2;
+        System.out.println(cem);
+    int ferg=num1-num2;
+        System.out.println(ferg);
+    int bolme=num1/num2;
+        System.out.println(bolme);
+    int vurma=num1*num2;
+        System.out.println(vurma);
+    int qaliq=num1%num2;
+        System.out.println(qaliq);
+
+}
