745d0d2a3b3c03cddff8162d75a0ec09ad10cee2
Aynurmm <aynurmammadova09@gmail.com>
2024-10-26 18:25:13 +0400

    commit

--- src/main/java/az/academy/turing/Operators.java
+++ src/main/java/az/academy/turing/Operators.java
@@ -3,18 +3,18 @@ package az.academy.turing;
 public class Operators {
     public static void main(String[] args) {
 
-//    int num1=12;
-//    int num2=4;
-//    int cem=num1+num2;
-//    System.out.println(cem);
-//    int ferg=num1-num2;
-//        System.out.println(ferg);
-//    int bolme=num1/num2;
-//        System.out.println(bolme);
-//    int vurma=num1*num2;
-//        System.out.println(vurma);
-//    int qaliq=num1%num2;
-//        System.out.println(qaliq);
+    int num1=12;
+    int num2=4;
+    int cem=num1+num2;
+    System.out.println(cem);
+    int ferg=num1-num2;
+        System.out.println(ferg);
+    int bolme=num1/num2;
+        System.out.println(bolme);
+    int vurma=num1*num2;
+        System.out.println(vurma);
+    int qaliq=num1%num2;
+        System.out.println(qaliq);
 
 
         int a=36;
