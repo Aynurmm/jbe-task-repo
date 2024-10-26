f46e0378fc2a9f1c0f4fc2dee946be873d4cf2db
Aynurmm <aynurmammadova09@gmail.com>
2024-10-26 18:48:36 +0400

    commit

--- src/main/java/az/academy/turing/Operators.java
+++ src/main/java/az/academy/turing/Operators.java
@@ -1,27 +1,36 @@
 package az.academy.turing;
 
+import java.util.Scanner;
+
 public class Operators {
     public static void main(String[] args) {
-
-    int num1=12;
-    int num2=4;
-    int cem=num1+num2;
-    System.out.println(cem);
-    int ferg=num1-num2;
-        System.out.println(ferg);
-    int bolme=num1/num2;
-        System.out.println(bolme);
-    int vurma=num1*num2;
-        System.out.println(vurma);
-    int qaliq=num1%num2;
-        System.out.println(qaliq);
+//
+//    int num1=12;
+//    int num2=4;
+//    int cem=num1+num2;
+//    System.out.println(cem);
+//    int ferg=num1-num2;
+//        System.out.println(ferg);
+//    int bolme=num1/num2;
+//        System.out.println(bolme);
+//    int vurma=num1*num2;
+//        System.out.println(vurma);
+//    int qaliq=num1%num2;
+//        System.out.println(qaliq);
 
 
         int a=36;
         int b=5;
         float f=(float)a/b;
-        System.out.printf("%f\n", f );
-        System.out.println(f);
+//        System.out.printf("%f\n", f );
+//        System.out.println(f);
+
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("eded daxil edin");
+        int number= sc.nextInt();
+        String result = (number % 2 == 0) ? "Sayı cutdur." : "Sayı tekdir.";
+        System.out.println(result);
     }
 }
 
